2b12c6d83bbeda1c9cd1a11b03667e1ee0356806
@Jurczak <nickolas.jurczak@gmail.com>
2018-03-14 14:38:52 -0400

    formy form

---
+++ Week 3/first-javascript-example/form.js
@@ -0,0 +1,13 @@
+function addTree(){
+
+    var id = document.getElementById("t_id").value;
+    var species = document.getElementById("t_species").value;
+    var age = document.getElementById("t_age").value;
+    var height = document.getElementById("t_height").value;
+
+    console.log("id: "+id);
+    console.log("species: "+species);
+    console.log("age: "+age);
+    console.log("height: "+height);
+
+}
