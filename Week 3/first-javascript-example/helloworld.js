e4ceb3d6b9c71f1e134e78bc2ff79cbf84ca1522
@Jurczak <nickolas.jurczak@gmail.com>
2018-03-14 12:04:05 -0400

    es6 examples

--- Week 3/first-javascript-example/helloworld.js
+++ Week 3/first-javascript-example/helloworld.js
@@ -25,4 +25,67 @@ function changeString(myVar){
 
 var myString = "Old value";
 changeString(myString);
+
+
 console.log("Value outside method " + myString);
+
+
+//closure
+var y = function(x){
+
+    var value = x;
+
+    return function(){
+
+        return value;
+
+    }
+
+}(10);
+
+console.log(y());
+
+//IIFE (immediatly invoked function expression)
+y = function(){
+
+    var a = 99;
+
+    console.log(a);
+
+}();
+
+//block scoping with let
+{
+
+    let a = 77;
+    const x = 11;
+    //x = 77;
+
+}
+
+class myClass{
+
+    constructor(a, b){
+
+        this.name = a;
+        this.value = b;
+
+    }
+
+    getName(){
+
+        return this.name;
+
+    }
+
+    setName(n){
+
+        this.name = n;
+
+    }
+
+}
+
+var myObject = new myClass("hi", "bye");
+
+console.log(myObject.getName());
