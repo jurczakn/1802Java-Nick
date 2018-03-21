840c9baa5c6483dbf3a3639f86b53bd14384990b
@Jurczak <nickolas.jurczak@gmail.com>
2018-03-21 12:15:54 -0400

    config context

---
+++ Week4/HelloServlet/src/main/java/com/revature/servlet/ContextServlet.java
@@ -0,0 +1,36 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ContextServlet
+ */
+public class ContextServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String conValue = getServletContext().getInitParameter("name2");
+		System.out.println(conValue);
+		conValue = getServletConfig().getServletContext().getInitParameter("name2");
+		System.out.println(conValue);
+		String value = getInitParameter("name1");
+		System.out.println(value);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
